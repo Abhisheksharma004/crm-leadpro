0fc3ad0f5a72c721d4cc84967c79266c50d354db
Abhishheksharma004 <abhishekkumarranjan965@gmail.com>
2026-08-24 15:30:15 +0530

    fix: update help test assertion and CI workflow

--- tests/Feature/HelpTest.php
+++ tests/Feature/HelpTest.php
@@ -9,7 +9,7 @@ it('shows the help page to an authenticated admin', function () {
         ->assertSee('Help & Resources')
         ->assertSee('Cloud Hosting')
         ->assertSee('Extensions')
-        ->assertSee('krayincrm.com/cloud-hosting')
+        ->assertSee('leadpro.co.in')
         ->assertSee('Still need a hand?')
         ->assertSee('Community Forums')
         ->assertSee('Video Tutorials');
