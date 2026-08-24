9b8a0aad6d1b79eeb9f086dfb1f22ba36fcb8988
Abhishheksharma004 <abhishekkumarranjan965@gmail.com>
2026-08-24 14:30:41 +0530

    first commit

---
+++ packages/Webkul/Admin/src/Config/support.php
@@ -0,0 +1,70 @@
+<?php
+
+return [
+    'services' => [
+        [
+            'icon' => 'cloud',
+            'title' => 'admin::app.help.index.services.cloud-hosting.title',
+            'description' => 'admin::app.help.index.services.cloud-hosting.description',
+            'url' => 'https://leadpro.co.in/',
+            'url_label' => 'leadpro.co.in',
+        ],
+        [
+            'icon' => 'support',
+            'title' => 'admin::app.help.index.services.support.title',
+            'description' => 'admin::app.help.index.services.support.description',
+            'url' => 'https://virosentrepreneurs.com/contact',
+            'url_label' => 'virosentrepreneurs.com/contact',
+        ],
+        [
+            'icon' => 'extensions',
+            'title' => 'admin::app.help.index.services.extensions.title',
+            'description' => 'admin::app.help.index.services.extensions.description',
+            'url' => 'https://leadpro.co.in/',
+            'url_label' => 'leadpro.co.in',
+        ],
+    ],
+
+    'resources' => [
+        [
+            'icon' => 'docs',
+            'title' => 'admin::app.help.index.resources.user-docs.title',
+            'description' => 'admin::app.help.index.resources.user-docs.description',
+            'url' => 'https://leadpro.co.in/',
+            'url_label' => 'leadpro.co.in/docs',
+        ],
+        [
+            'icon' => 'docs',
+            'title' => 'admin::app.help.index.resources.dev-docs.title',
+            'description' => 'admin::app.help.index.resources.dev-docs.description',
+            'url' => 'https://leadpro.co.in/',
+            'url_label' => 'leadpro.co.in/dev',
+        ],
+        [
+            'icon' => 'api',
+            'title' => 'admin::app.help.index.resources.api-docs.title',
+            'description' => 'admin::app.help.index.resources.api-docs.description',
+            'url' => 'https://leadpro.co.in/',
+            'url_label' => 'leadpro.co.in/api',
+        ],
+    ],
+
+    'community' => [
+        [
+            'icon' => 'community',
+            'title' => 'admin::app.help.index.community.forums.title',
+            'description' => 'admin::app.help.index.community.forums.description',
+            'url' => 'https://virosentrepreneurs.com',
+            'link_label' => 'admin::app.help.index.community.forums.link',
+        ],
+        [
+            'icon' => 'video',
+            'title' => 'admin::app.help.index.community.tutorials.title',
+            'description' => 'admin::app.help.index.community.tutorials.description',
+            'url' => 'https://virosentrepreneurs.com',
+            'link_label' => 'admin::app.help.index.community.tutorials.link',
+        ],
+    ],
+
+    'contact_url' => 'https://virosentrepreneurs.com/',
+];
